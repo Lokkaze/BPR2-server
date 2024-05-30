2edd3477a40c78535a4a914e4d696154b8cffeaa
Lokkaze <74102422+Lokkaze@users.noreply.github.com>
2024-05-30 16:24:35 +0200

    Polishment

--- src/main/java/com/example/bpr2server/utils/CorsConfig.java
+++ src/main/java/com/example/bpr2server/utils/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.bpr2server.config;
+package com.example.bpr2server.utils;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
